09301eb586a31cb97a6c065d05279910bc123ad8
foxtrotxray <kyleskycollins@gmail.com>
2019-07-08 12:34:32 -0700

    Prevent Sql injection with prepared statements over template literals

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ server.use(express.urlencoded({ extended: true }))
 
 server.get('/:listing/:query', function (req, res) {
   console.log('got a GET with a query!');
-  let result = db.connection().query(`select * from reviews where review_content like '%${req.params.query}%' AND listings_id = ${req.params.listing};
-  `, function (err, results, fields) {
+  let result = db.connection().execute(`select * from reviews where review_content like ?  AND listings_id = ?;
+  `,[`%${req.params.query}%`, req.params.listing], function (err, results, fields) {
     console.log(req.params);
 
     res.json(results)
@@ -15,7 +15,7 @@ server.get('/:listing/:query', function (req, res) {
 });
 server.get('/:listing', function (req, res) {
   console.log('got a GET!');
-  let result = db.connection().query(`SELECT * FROM reviews WHERE listings_id = ${req.params.listing}`, function (err, results, fields) {
+  let result = db.connection().execute(`SELECT * FROM reviews WHERE listings_id = ?`,[req.params.listing], function (err, results, fields) {
     console.log(req.params);
 
     res.json(results)
