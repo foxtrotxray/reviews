ddd7fd8052c2be0bb49e3f59bdb4a1f2e97e11db
foxtrotxray <kyleskycollins@gmail.com>
2019-07-08 18:39:21 -0700

    Add babel to the package; employ webpack for bundling; create a HTML/CSS skeleton; render react 'hello world'

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const entryPoint = path.resolve(__dirname, 'client', 'src', 'index.jsx');
+const outputPoint = path.resolve(__dirname, 'public');
+
+
+module.exports = {
+  entry: entryPoint,
+  output: {
+    filename: 'bundle.js',
+    path: outputPoint
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: entryPoint,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ['@babel/preset-react'],
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  node: 'current',
+                }
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  }
+};
