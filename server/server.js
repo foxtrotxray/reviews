93230bb3b945a0e9b5c68308d5ebb69d6c4933cc
foxtrotxray <kyleskycollins@gmail.com>
2019-07-09 13:51:16 -0700

    Refine router to retrieve data about the listing in question

--- server/server.js
+++ server/server.js
@@ -3,26 +3,37 @@ const express = require('express');
 let server = express();
 server.use(express.static(__dirname + '/../public'));
 server.use(express.urlencoded({ extended: true }))
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 server.get('/:listing/:query', function (req, res) {
   console.log('got a GET with a query!');
   let result = db.connection().execute(`select * from reviews where reply_content like ? AND listings_id = ? OR review_content like ? AND listings_id = ?;
-  `,[`%${req.params.query}%`,req.params.listing ,`%${req.params.query}%`, req.params.listing], function (err, results, fields) {
+  `,[`%${req.params.query}%`,req.params.listing ,`%${req.params.query}%`, req.params.listing], function (err, results1, fields)
+  {
+    let secondSelect = db.connection().execute(`SELECT * FROM listings WHERE id = ?`, [req.params.listing], function (err, results2, fields) {
+      let output = Object.assign(results1, results2)
+      // if (Object.keys(output).length <= 1) {
+      //   res.json('No reviews match your search!')
+      // } else {
+        res.json(output)
+      // }
+    })
+
     console.log(req.params);
-    if (results.length === 0) {
-      res.json('No reviews match your search!')
-    } else {
-      res.json(results)
-    }
   })
 });
 server.get('/:listing', function (req, res) {
   console.log('got a GET!');
   if(req.params.listing <= 100) {
-    let result = db.connection().execute(`SELECT * FROM reviews WHERE listings_id = ?`,[req.params.listing], function (err, results, fields) {
-      console.log(req.params);
-
-      res.json(results)
+    console.log(req.params);
+    let result = db.connection().execute(`SELECT * FROM reviews WHERE listings_id = ?`,[req.params.listing], function (err, results1, fields) {
+      let secondSelect = db.connection().execute(`SELECT * FROM listings WHERE id = ?`, [req.params.listing], function (err, results2, fields) {
+        res.json(Object.assign(results1, results2))
+      })
     })
   } else {
     console.log('404!')
@@ -31,3 +42,4 @@ server.get('/:listing', function (req, res) {
 });
 
 module.exports = server
+// SELECT * FROM reviews RIGHT JOIN listings ON reviews.listing_id = listings.id WHERE reviews.listings_id = ?
