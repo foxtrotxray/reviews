c038a5b85fde729c04904038a3060875514370cf
foxtrotxray <kyleskycollins@gmail.com>
2019-07-10 13:03:36 -0700

    update 'review' component to conditionally render owner responses; update seed script to randomly produce values or null

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -8,19 +8,31 @@ class Review extends React.Component {
     }
   }
   render() {
-    return(
-      <div>
-        <img src={this.props.reviewData.icon_url} width="48px" height="48px" ></img>
-        <span>{this.props.reviewData.author}</span>
-        <span>{this.props.reviewData.review_date}</span>
-        <div>{this.props.reviewData.review_content}</div>
+    if (this.props.reviewData.reply_content === null) {
+      return(
+        <div>
+          <img src={this.props.reviewData.icon_url} width="48px" height="48px" ></img>
+          <span>{this.props.reviewData.author}</span>
+          <span>{this.props.reviewData.review_date}</span>
+          <div>{this.props.reviewData.review_content}</div>
+        </div>
+      )
+    } else {
+      return(
+        <div>
+          <img src={this.props.reviewData.icon_url} width="48px" height="48px" ></img>
+          <span>{this.props.reviewData.author}</span>
+          <span>{this.props.reviewData.review_date}</span>
+          <div>{this.props.reviewData.review_content}</div>
 
-        <img src={this.props.listingData.owner_icon_url} width="48px" height="48px" ></img>
-        <span>{this.props.listingData.owner_name}</span>
-        <span>{this.props.reviewData.reply_date}</span>
-        <div>{this.props.reviewData.reply_content}</div>
-      </div>
-    )
+          <img src={this.props.listingData.owner_icon_url} width="48px" height="48px" ></img>
+          <span>{this.props.listingData.owner_name}</span>
+          <span>{this.props.reviewData.reply_date}</span>
+          <div>{this.props.reviewData.reply_content}</div>
+        </div>
+      )
+
+    }
   }
 }
 export default Review
